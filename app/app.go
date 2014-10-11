e124dbbc33f79d2dc099525b93aa68b5df7b7796
Andriy Romanov <judo.ras@gmail.com>
2014-10-11 14:36:16 -0700

    Initial commit

---
+++ app/app.go
@@ -0,0 +1,14 @@
+package app
+
+import (
+    "fmt"
+    "net/http"
+)
+
+func init() {
+    http.HandleFunc("/", handler)
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprint(w, "Hello, world from app")
+}
---
+++ mobile-frontend/mobile-frontend.go
@@ -0,0 +1,14 @@
+package mobile
+
+import (
+    "fmt"
+    "net/http"
+)
+
+func init() {
+    http.HandleFunc("/", handler)
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprint(w, "Hello, world from mobile")
+}
